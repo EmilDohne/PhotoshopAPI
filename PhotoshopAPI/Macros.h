590f178f7ecfed50c5cb59708342c96a5cbe91cd
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2023-10-20 16:26:05 +0200

    Add project files.

---
+++ PhotoshopAPI/Macros.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define PSAPI_NAMESPACE_BEGIN namespace PhotoshopAPI {
+#define PSAPI_NAMESPACE_END }
---
+++ PhotoshopAPI/PhotoshopFile/PhotoshopFile.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../Macros.h"
+
+
+PSAPI_NAMESPACE_BEGIN
+
+
+struct PhotoshopFile
+{
+
+};
+
+
+PSAPI_NAMESPACE_END
