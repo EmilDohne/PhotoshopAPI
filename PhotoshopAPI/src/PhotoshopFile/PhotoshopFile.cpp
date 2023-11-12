0ad4c8ad61307adb9845ffd3ae1290fdc8723303
emild <emild@DESKTOP-VLBKAQ3>
2023-11-12 11:07:22 +0100

    remove unneeded param

--- PhotoshopAPI/src/PhotoshopFile/PhotoshopFile.cpp
+++ PhotoshopAPI/src/PhotoshopFile/PhotoshopFile.cpp
@@ -14,7 +14,7 @@ bool PhotoshopFile::read(File& document)
 	bool wasSuccessful = true;
 
 	wasSuccessful = wasSuccessful && m_Header.read(document);
-	wasSuccessful = wasSuccessful && m_ColorModeData.read(document, m_Header);
+	wasSuccessful = wasSuccessful && m_ColorModeData.read(document);
 	wasSuccessful = wasSuccessful && m_ImageResources.read(document, m_ColorModeData.m_Offset + m_ColorModeData.m_Size);
 	wasSuccessful = wasSuccessful && m_LayerMaskInfo.read(document, m_Header, m_ImageResources.m_Offset + m_ImageResources.m_Size);
 
