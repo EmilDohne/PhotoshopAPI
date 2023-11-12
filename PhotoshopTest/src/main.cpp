bd49e5f89f0d598cda043bc11a9b34848908b6e9
emild <emild@DESKTOP-VLBKAQ3>
2023-11-12 11:17:03 +0100

    Add test cases and update todo

--- PhotoshopTest/src/main.cpp
+++ PhotoshopTest/src/main.cpp
@@ -8,8 +8,43 @@
 
 std::vector<std::filesystem::path> relPaths =
 {
+	"\\documents\\CMYK_8bit.psd",
+	"\\documents\\CMYK_8bit.psb",
+	"\\documents\\CMYK_16bit.psd",
+	"\\documents\\CMYK_16bit.psb",
+
+	"\\documents\\Grayscale_8bit.psd",
+	"\\documents\\Grayscale_8bit.psb",
+	"\\documents\\Grayscale_16bit.psd",
+	"\\documents\\Grayscale_16bit.psb",
+	"\\documents\\Grayscale_32bit.psd",
+	"\\documents\\Grayscale_32bit.psb",
+
+	"\\documents\\Groups_8bit.psd",
+	"\\documents\\Groups_8bit.psb",
+	"\\documents\\Groups_16bit.psd",
+	"\\documents\\Groups_16bit.psb",
+	"\\documents\\Groups_32bit.psd",
+	"\\documents\\Groups_32bit.psb",
+
+	"\\documents\\Indexed_8bit.psd",
+	"\\documents\\Indexed_8bit.psb",
+
 	"\\documents\\Masks_8bit.psd",
-	"\\documents\\Masks_8bit.psb"
+	"\\documents\\Masks_8bit.psb",
+
+	"\\documents\\SingleLayer_8bit.psd",
+	"\\documents\\SingleLayer_8bit.psb",
+	"\\documents\\SingleLayer_8bit_MaximizeCompatibilityOff.psd",
+	"\\documents\\SingleLayer_8bit_MaximizeCompatibilityOff.psb",
+	"\\documents\\SingleLayer_16bit.psd",
+	"\\documents\\SingleLayer_16bit.psb",
+	"\\documents\\SingleLayer_16bit_MaximizeCompatibilityOff.psd",
+	"\\documents\\SingleLayer_16bit_MaximizeCompatibilityOff.psb",
+	"\\documents\\SingleLayer_32bit.psd",
+	"\\documents\\SingleLayer_32bit.psb",
+	"\\documents\\SingleLayer_32bit_MaximizeCompatibilityOff.psd",
+	"\\documents\\SingleLayer_32bit_MaximizeCompatibilityOff.psb",
 };
 
 
--- TODO.md
+++ TODO.md
@@ -2,13 +2,13 @@
 
 ## Project
 
-- [ ] Generate CMake project for PhotoshopAPI
+- [X] Generate CMake project for PhotoshopAPI
 - [ ] Include blosc2 compression library
 - [ ] Add README
 
 ## Testing
 
-- [ ] Add wide-range of test cases for testing based on photoshop files (both psd and psb)
+- [X] Add wide-range of test cases for testing based on photoshop files (both psd and psb)
 - [ ] Add unit testing for low-level components such as reading from a binary file
  
 ## PhotoshopFile
@@ -18,8 +18,8 @@
 - [X] Read Image Resources
 - [ ] Read Layer and Mask information
     - [X] Read Global Layer Mask Info
-    - [ ] Read Layer Records
-        - [ ] Add parsing of tagged blocks 
+    - [X] Read Layer Records
+        - [X] Add parsing of tagged blocks 
     - [ ] Read Channel Image Data
         - [ ] Compress using blosc2
     - [ ] Read Additional Layer Information
