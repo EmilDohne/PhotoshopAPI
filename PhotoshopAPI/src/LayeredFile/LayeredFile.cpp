0c514d5054b9ab132a0b150c57db30895d4d6676
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2023-11-26 12:14:08 +0100

    Add layer identification function

--- PhotoshopAPI/src/LayeredFile/LayeredFile.cpp
+++ PhotoshopAPI/src/LayeredFile/LayeredFile.cpp
@@ -1,13 +1,19 @@
 #include "LayeredFile.h"
 
 #include "Macros.h"
-#include "Layer.h"
 #include "PhotoshopFile/PhotoshopFile.h"
 #include "Struct/TaggedBlock.h"
+#include "LayerTypes/Layer.h"
+#include "LayerTypes/ImageLayer.h"
+#include "LayerTypes/GroupLayer.h"
+#include "LayerTypes/AdjustmentLayer.h"
+#include "LayerTypes/ArtboardLayer.h"
+#include "LayerTypes/SectionDividerLayer.h"
+#include "LayerTypes/ShapeLayer.h"
+#include "LayerTypes/SmartObjectLayer.h"
+#include "LayerTypes/TextLayer.h"
 
-
-#include <ranges>
-#include <span>
+#include <vector>
 
 
 PSAPI_NAMESPACE_BEGIN
@@ -25,8 +31,61 @@ LayeredFile<T>::LayeredFile(std::unique_ptr<PhotoshopFile> file)
 	// Take ownership of document
 	std::unique_ptr<PhotoshopFile> document = std::move(file);
 
+	m_BitDepth = document->m_Header.m_Depth;
+	m_ColorMode = document->m_Header.m_ColorMode;
+	m_Width = document->m_Header.m_Width;
+	m_Height = document->m_Header.m_Height;
+
 	// Build the layer hierarchy
 	m_Layers = LayeredFileImpl::buildLayerHierarchy<T>(std::move(document));
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+LayeredFile<T>::LayeredFile(Enum::ColorMode colorMode, uint64_t width, uint64_t height) requires std::same_as<T, uint8_t>
+{
+	m_BitDepth = Enum::BitDepth::BD_8;
+	m_ColorMode = colorMode;
+	m_Width = width;
+	m_Height = height;
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+LayeredFile<T>::LayeredFile(Enum::ColorMode colorMode, uint64_t width, uint64_t height) requires std::same_as<T, uint16_t>
+{
+	m_BitDepth = Enum::BitDepth::BD_16;
+	m_ColorMode = colorMode;
+	m_Width = width;
+	m_Height = height;
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+LayeredFile<T>::LayeredFile(Enum::ColorMode colorMode, uint64_t width, uint64_t height) requires std::same_as<T, float32_t>
+{
+	m_BitDepth = Enum::BitDepth::BD_32;
+	m_ColorMode = colorMode;
+	m_Width = width;
+	m_Height = height;
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+LayeredFile<T>::LayeredFile(Enum::BitDepth bitDepth, Enum::ColorMode colorMode, uint64_t width, uint64_t height)
+{
+	m_BitDepth = bitDepth;
+	m_ColorMode = colorMode;
+	m_Width = width;
+	m_Height = height;
 }
 
 
@@ -36,6 +95,9 @@ template <typename T>
 std::vector<layerVariant<T>> LayeredFileImpl::buildLayerHierarchy(std::unique_ptr<PhotoshopFile> file)
 {
 	auto& layerRecords = file->m_LayerMaskInfo.m_LayerInfo.m_LayerRecords;
+	auto& channelImageData = file->m_LayerMaskInfo.m_LayerInfo.m_ChannelImageData;
+
+	if (layerRecords.size() != channelImageData.size())
 
 	// Extract and iterate the layer records. We do this in reverse as Photoshop stores the layers in reverse
 	// For example, imagine this layer structure:
@@ -50,44 +112,183 @@ std::vector<layerVariant<T>> LayeredFileImpl::buildLayerHierarchy(std::unique_pt
 	// Group
 	//
 	// Layer divider in this case being an empty layer with a 'lsct' tagged block with Type set to 3
-	std::vector<LayerRecord>::reverse_iterator layerRecordsIter = layerRecords.rbegin();
-	std::vector<layerVariant<T>> root = buildLayerHierarchyRecurse<T>(layerRecords, layerRecordsIter);
+	auto layerRecordsIterator = layerRecords.rbegin();
+	auto channelImageDataIterator = channelImageData.rbegin();
+	std::vector<layerVariant<T>> root = buildLayerHierarchyRecurse<T>(layerRecords, channelImageData, layerRecordsIterator, channelImageDataIterator);
 
 	return root;
 }
 
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 template <typename T>
-std::vector<layerVariant<T>> LayeredFileImpl::buildLayerHierarchyRecurse(const std::vector<LayerRecord>& layerRecords, std::vector<LayerRecord>::reverse_iterator& layerRecordsIterator)
+std::vector<layerVariant<T>> LayeredFileImpl::buildLayerHierarchyRecurse(
+	const std::vector<LayerRecord>& layerRecords,
+	const std::vector<std::shared_ptr<ChannelImageData<T>>>& channelImageData,
+	std::vector<LayerRecord>::reverse_iterator& layerRecordsIterator,
+	std::vector<std::shared_ptr<ChannelImageData<T>>>::reverse_iterator& channelImageDataIterator)
 {
 	std::vector<layerVariant<T>> root;
-	while (layerRecordsIterator != layerRecords.rend())
+
+	// Iterate the layer records and channelImageData. These are always the same size
+	while (layerRecordsIterator != layerRecords.rend() && channelImageDataIterator != channelImageData.rend())
 	{
 		const auto& layerRecord = *layerRecordsIterator;
+		const auto channelImagePtr = *channelImageDataIterator;
 		auto& additionalLayerInfo = layerRecord.m_AdditionalLayerInfo.value();
 
 		auto sectionDivider = additionalLayerInfo.getTaggedBlock<TaggedBlock::LayerSectionDivider>(Enum::TaggedBlockKey::lrSectionDivider);
-		if (sectionDivider.has_value())
+
+		if (sectionDivider.has_value() && (sectionDivider.value()->m_Type == Enum::SectionDivider::ClosedFolder || sectionDivider.value()->m_Type == Enum::SectionDivider::OpenFolder))
 		{
-			if (sectionDivider.value()->m_Type == Enum::SectionDivider::ClosedFolder || sectionDivider.value()->m_Type == Enum::SectionDivider::OpenFolder)
-			{
-				// Create a new group layer
-				GroupLayer<T> groupLayer(layerRecord);
+			// Create a new group layer
+			GroupLayer<T> groupLayer(layerRecord);
 
-				// Recurse down a level while incrementing to go to the next layer
-				groupLayer.m_Layers = buildLayerHierarchyRecurse<T>(layerRecords, ++layerRecordsIterator);
+			// Recurse down a level while incrementing to go to the next layer
+			groupLayer.m_Layers = buildLayerHierarchyRecurse<T>(layerRecords, ++layerRecordsIterator, channelImageData, ++channelImageDataIterator);
 
-				root.push_back(groupLayer);
-			}
-			else if (sectionDivider.value()->m_Type == Enum::SectionDivider::BoundingSection)
-			{
-				// We have reached the end of the current nested section therefore we return the current root object we hold;
-				return root;
-			}
+			root.push_back(groupLayer);
 		}
-		// Increment the counter to go to the next layer
+		else if (sectionDivider.has_value() && (sectionDivider.value()->m_Type == Enum::SectionDivider::BoundingSection))
+		{
+			// We have reached the end of the current nested section therefore we return the current root object we hold;
+			return root;
+		}
+		else
+		{
+			// For now we just parse the rest as image layers, but if 
+		}
+		// Increment the iterators to go to the next layer
+		++channelImageDataIterator;
 		++layerRecordsIterator;
 	}
 	return root;
 }
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+layerVariant<T> LayeredFileImpl::identifyLayerType(const LayerRecord& layerRecord, std::shared_ptr<ChannelImageData<T>> channelImageData)
+{
+	auto& additionalLayerInfo = layerRecord.m_AdditionalLayerInfo.value();
+
+	// Check for GroupLayer, ArtboardLayer or SectionDividerLayer
+	auto sectionDividerTaggedBlock = additionalLayerInfo.getTaggedBlock<TaggedBlock::LayerSectionDivider>(Enum::TaggedBlockKey::lrSectionDivider);
+	if (sectionDividerTaggedBlock.has_value())
+	{
+		if (sectionDividerTaggedBlock.value()->m_Type == Enum::SectionDivider::ClosedFolder
+			|| sectionDividerTaggedBlock.value()->m_Type == Enum::SectionDivider::OpenFolder)
+		{
+			// This may actually house not only a group layer, but potentially also an artboard layer which we check for first
+			// These are, as of yet, unsupported. Therefore we simply return an empty container
+			auto artboardTaggedBlock = additionalLayerInfo.getTaggedBlock<TaggedBlock::Generic>(Enum::TaggedBlockKey::lrArtboard);
+			if (artboardTaggedBlock.has_value())
+			{
+				return ArtboardLayer();
+			}
+			return GroupLayer(layerRecord, channelImageData);
+		}
+		else if (sectionDividerTaggedBlock.value()->m_Type == Enum::SectionDivider::BoundingSection)
+		{
+			return SectionDividerLayer();
+		}
+		// If it is Enum::SectionDivider::Any this is just any other type of layer
+		// we do not need to worry about checking for correctness here as the tagged block takes care of that 
+	}
+
+	// Check for Text Layers
+	auto typeToolTaggedBlock = additionalLayerInfo.getTaggedBlock<TaggedBlock::Generic>(Enum::TaggedBlockKey::lrTypeTool);
+	if (typeToolTaggedBlock.has_value())
+	{
+		return TextLayer<T>();
+	}
+
+	// Check for Smart Object Layers
+	auto smartObjectTaggedBlock = additionalLayerInfo.getTaggedBlock<TaggedBlock::Generic>(Enum::TaggedBlockKey::lrSmartObject);
+	if (typeToolTaggedBlock.has_value())
+	{
+		return SmartObjectLayer<T>();
+	}
+
+	// Check if it is one of many adjustment layers
+	// We do not currently implement these but it would be worth investigating
+	{
+#define getGenericTaggedBlock additionalLayerInfo.getTaggedBlock<TaggedBlock::Generic>
+
+		auto blackAndWhiteTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjBlackandWhite);
+		auto gradientTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjGradient);
+		auto invertTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjInvert);
+		auto patternFillTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjPattern);
+		auto posterizeTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjPosterize);
+		auto solidColorTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjSolidColor);
+		auto thresholdTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjThreshold);
+		auto vibranceTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjVibrance);
+		auto brightnessContrastTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjBrightnessContrast);
+		auto colorBalanceTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjColorBalance);
+		auto colorLookupTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjColorLookup);
+		auto channelMixerTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjChannelMixer);
+		auto curvesTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjCurves);
+		auto gradientMapTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjGradientMap);
+		auto exposureTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjExposure);
+		auto newHueSatTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjNewHueSat);
+		auto oldHueSatTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjOldHueSat);
+		auto levelsTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjLevels);
+		auto photoFilterTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjPhotoFilter);
+		auto selectiveColorTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::adjSelectiveColor);
+
+		if (blackAndWhiteTaggedBlock.has_value()
+			|| gradientTaggedBlock.has_value()
+			|| invertTaggedBlock.has_value()
+			|| patternFillTaggedBlock.has_value()
+			|| posterizeTaggedBlock.has_value()
+			|| solidColorTaggedBlock.has_value()
+			|| thresholdTaggedBlock.has_value()
+			|| vibranceTaggedBlock.has_value()
+			|| brightnessContrastTaggedBlock.has_value()
+			|| colorBalanceTaggedBlock.has_value()
+			|| colorLookupTaggedBlock.has_value()
+			|| channelMixerTaggedBlock.has_value()
+			|| curvesTaggedBlock.has_value()
+			|| gradientMapTaggedBlock.has_value()
+			|| exposureTaggedBlock.has_value()
+			|| newHueSatTaggedBlock.has_value()
+			|| oldHueSatTaggedBlock.has_value()
+			|| levelsTaggedBlock.has_value()
+			|| photoFilterTaggedBlock.has_value()
+			|| selectiveColorTaggedBlock.has_value()
+			)
+		{
+			return AdjustmentLayer<T>();
+		}
+#undef getGenericTaggedBlock
+	}
+	
+	// Now the layer could only be one of two more. A shape or pixel layer (Note files written before CS6 could fail this shape layer check here
+	{
+#define getGenericTaggedBlock additionalLayerInfo.getTaggedBlock<TaggedBlock::Generic>
+		
+		{
+			auto vecOriginDataTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::vecOriginData);
+			auto vecMaskSettingTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::vecMaskSettings);
+			auto vecStrokeDataTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::vecStrokeData);
+			auto vecStrokeContentDataTaggedBlock = getGenericTaggedBlock(Enum::TaggedBlockKey::vecStrokeContentData);
+
+			if (vecOriginDataTaggedBlock.has_value()
+				|| vecMaskSettingTaggedBlock.has_value()
+				|| vecStrokeDataTaggedBlock.has_value()
+				|| vecStrokeContentDataTaggedBlock.has_value()
+				)
+			{
+				return ShapeLayer<T>();
+			}
+#undef getGenericTaggedBlock
+		}
+	}
+
+	return ImageLayer<T>(layerRecord, channelImageData);
+}
+
 
 PSAPI_NAMESPACE_END
--- PhotoshopAPI/src/LayeredFile/LayeredFile.h
+++ PhotoshopAPI/src/LayeredFile/LayeredFile.h
@@ -2,11 +2,18 @@
 
 #include "Macros.h"
 #include "PhotoshopFile/PhotoshopFile.h"
-#include "Layer.h"
-#include "ImageLayer.h"
-#include "GroupLayer.h"
+#include "PhotoshopFile/LayerAndMaskInformation.h"
 
-#include <span>
+#include "LayerTypes/Layer.h"
+#include "LayerTypes/ImageLayer.h"
+#include "LayerTypes/GroupLayer.h"
+#include "LayerTypes/AdjustmentLayer.h"
+#include "LayerTypes/ArtboardLayer.h"
+#include "LayerTypes/SectionDividerLayer.h"
+#include "LayerTypes/ShapeLayer.h"
+#include "LayerTypes/SmartObjectLayer.h"
+#include "LayerTypes/TextLayer.h"
+
 
 PSAPI_NAMESPACE_BEGIN
 
@@ -18,20 +25,50 @@ struct LayeredFile
 	// The root layers in the file, they may contain multiple levels of sub-layers
 	std::vector<layerVariant<T>> m_Layers;
 
+	Enum::BitDepth m_BitDepth = Enum::BitDepth::BD_8;
+	Enum::ColorMode m_ColorMode = Enum::ColorMode::RGB;	// Currently we only support RGB but this may change in the future
+	uint64_t m_Width = 0u;
+	uint64_t m_Height = 0u;
+
 	// Generate a LayeredFile instance from a pointer to a photoshop file, taking ownership of it 
 	// and discarding it once we are done with it. This involves transferring from a flat layer hierarchy
 	// to a layered file using the lrSectionDivider taggedBlock to identify layer breaks
 	LayeredFile(std::unique_ptr<PhotoshopFile> file);
+
+	// Generate an empty LayeredFile object whose layers are yet to be populated
+	LayeredFile(Enum::ColorMode colorMode, uint64_t width, uint64_t height) requires std::same_as<T, uint8_t>;
+	LayeredFile(Enum::ColorMode colorMode, uint64_t width, uint64_t height) requires std::same_as<T, uint16_t>;
+	LayeredFile(Enum::ColorMode colorMode, uint64_t width, uint64_t height) requires std::same_as<T, float32_t>;
+
+	LayeredFile(Enum::BitDepth bitDepth, Enum::ColorMode colorMode, uint64_t width, uint64_t height);
 };
 
 
 namespace LayeredFileImpl
 {
+
+	// Build the layer hierarchy from a PhotoshopFile object using the Layer and Mask section with its LayerRecords and ChannelImageData subsections;
+	// Returns a vector of nested layer variants which can go to any depth
 	template <typename T>
 	std::vector<layerVariant<T>> buildLayerHierarchy(std::unique_ptr<PhotoshopFile> file);
 
+
+	// Recursively build a layer hierarchy using the LayerRecords, ChannelImageData and their respective reverse iterators
+	// See comments in buildLayerHierarchy on why we iterate in reverse
 	template <typename T>
-	std::vector<layerVariant<T>> buildLayerHierarchyRecurse(const std::vector<LayerRecord>& layerRecords, std::vector<LayerRecord>::reverse_iterator& layerRecordsIterator);
+	std::vector<layerVariant<T>> buildLayerHierarchyRecurse(
+		const std::vector<LayerRecord>& layerRecords,
+		const std::vector<std::shared_ptr<ChannelImageData<T>>>& channelImageData,
+		std::vector<LayerRecord>::reverse_iterator& layerRecordsIterator,
+		typename std::vector<std::shared_ptr<ChannelImageData<T>>>::reverse_iterator& channelImageDataIterator);
+
+
+	// Identify the type of layer the current layer record represents and return a layerVariant object (std::variant<ImageLayer, GroupLayer ...>)
+	// initialized with the given layer record and corresponding channel image data.
+	// This function was heavily inspired by the psd-tools library as they have the most coherent parsing of this information
+	template <typename T>
+	layerVariant<T> identifyLayerType(const LayerRecord& layerRecord, std::shared_ptr<ChannelImageData<T>> channelImageData);
+
 }
 
 PSAPI_NAMESPACE_END
