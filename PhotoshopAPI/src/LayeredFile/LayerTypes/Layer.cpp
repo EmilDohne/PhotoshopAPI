7c7a4b02ce5b3705c9c3d89298ac27521547332f
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2023-12-19 11:01:27 +0100

    Add utility functions to the base layer allowing for transfer from layer -> photoshopfile

--- PhotoshopAPI/src/LayeredFile/LayerTypes/Layer.cpp
+++ PhotoshopAPI/src/LayeredFile/LayerTypes/Layer.cpp
@@ -12,11 +12,15 @@
 
 PSAPI_NAMESPACE_BEGIN
 
+
 // Instantiate the template types for Layer
 template struct Layer<uint8_t>;
 template struct Layer<uint16_t>;
 template struct Layer<float32_t>;
 
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 template <typename T>
 Layer<T>::Layer(const LayerRecord& layerRecord, const ChannelImageData& channelImageData)
 {
@@ -36,10 +40,170 @@ Layer<T>::Layer(const LayerRecord& layerRecord, const ChannelImageData& channelI
 		}
 	}
 	m_Opacity = layerRecord.m_Opacity;
-	m_Width = static_cast<uint64_t>(layerRecord.m_Right) - layerRecord.m_Left;
-	m_Height = static_cast<uint64_t>(layerRecord.m_Bottom) - layerRecord.m_Top;
+	m_Width = layerRecord.m_Right - layerRecord.m_Left;
+	m_Height = layerRecord.m_Bottom - layerRecord.m_Top;
+	m_CenterX = (layerRecord.m_Left - layerRecord.m_Right) / 2;
+	m_CenterY = (layerRecord.m_Top - layerRecord.m_Bottom) / 2;
 }
 
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+std::optional<LayerRecords::LayerMaskData> Layer<T>::generateMaskData()
+{
+	auto lrMaskData = LayerRecords::LayerMaskData();
+
+	// We dont have support for vector masks so far so we do not consider them
+	lrMaskData.m_VectorMask = std::nullopt;
+	if (!m_LayerMask.has_value())
+	{
+		lrMaskData.m_LayerMask = std::nullopt;
+	}
+	else
+	{
+		LayerRecords::LayerMask lrMask = LayerRecords::LayerMask{};
+
+		int32_t top, left, bottom, right;
+		top		= m_LayerMask.value().maskData.m_CenterY - m_Height / 2;
+		left	= m_LayerMask.value().maskData.m_CenterX - m_Width / 2;
+		bottom	= m_LayerMask.value().maskData.m_CenterY + m_Height / 2;
+		right	= m_LayerMask.value().maskData.m_CenterX + m_Width / 2;
+		lrMaskData.m_Size += 16u;
+
+		uint8_t defaultColor = 0u;
+		lrMaskData.m_Size += 1u;
+
+		// This is the size for the mask bitflags
+		lrMaskData.m_Size += 1u;
+		// This is the size for the mask parameters
+		lrMaskData.m_Size += 1u;
+		bool hasMaskDensity = m_LayerMask.value().maskDensity.has_value();
+		uint8_t maskDensity = 0u;
+		if (hasMaskDensity)
+		{
+			maskFeather = m_LayerMask.value().maskDensity.value();
+			lrMaskData.m_Size += 1u;
+		}
+
+		bool hasMaskFeather = m_LayerMask.value().maskFeather.has_value();
+		float64_t maskFeather = 0.0f;
+		if (hasMaskFeather)
+		{
+			maskFeather = m_LayerMask.value().maskFeather.value();
+			lrMaskData.m_Size += 8u;
+
+		}
+
+		lrMask.m_Top = top;
+		lrMask.m_Left = left;
+		lrMask.m_Bottom = bottom;
+		lrMask.m_Right = right;
+		lrMask.m_Disabled = m_LayerMask.value().isDisabled;
+		lrMask.m_HasMaskParams = hasMaskDensity || hasMaskFeather;
+		lrMask.m_HasUserMaskDensity = hasMaskDensity;
+		lrMask.m_HasUserMaskFeather = hasMaskFeather;
+		if (hasMaskDensity)
+		{
+			lrMask.m_UserMaskDensity.emplace(maskDensity);
+		}
+		if (hasMaskFeather)
+		{
+			lrMask.m_UserMaskFeather.emplace(maskFeather);
+		}
+
+		lrMaskData.m_LayerMask.emplace(lrMask);
+	}
+	
+
+	std::optional<LayerRecords::LayerMaskData> lrMaskDataOpt;
+	lrMaskDataOpt.emplace(lrMaskData);
+	if (!lrMaskData.m_LayerMask.has_value() && !lrMaskData.m_VectorMask(has_value()))
+	{
+		return std::nullopt;
+	}
+	return lrMaskDataOpt;
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+std::tuple<int32_t, int32_t, int32_t, int32_t> Layer<T>::generateExtents()
+{
+	int32_t top		= m_CenterY - m_Height / 2;
+	int32_t left	= m_CenterX - m_Width / 2;
+	int32_t bottom	= m_CenterY + m_Height / 2;
+	int32_t right	= m_CenterX + m_Width / 2;
+
+	return std::make_tuple(top, left, bottom, right);
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+PascalString Layer<T>::generatePascalString()
+{
+	return PascalString(m_LayerName, 4u);
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+LayerRecords::LayerBlendingRanges Layer<T>::generateBlendingRanges(const Enum::ColorMode colorMode)
+{
+	using Data = std::vector<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>>;
+	LayerRecords::LayerBlendingRanges blendingRanges{};
+
+	// Note that all of these color modes seem to have an additional blending range for alpha which cannot be accessed
+	// through the photoshop ui. This might be for legacy reasons
+	if (colorMode == Enum::ColorMode::RGB)
+	{
+		Data sourceRanges;
+		Data destinationRanges;
+		for (int i = 0; i < 5u; ++i)
+		{
+			sourceRanges.push_back(std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(0u, 0u, 255u, 255u));
+			destinationRanges.push_back(std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(0u, 0u, 255u, 255u));
+			blendingRanges.m_Size += 8u;
+		}
+		blendingRanges.m_SourceRanges = sourceRanges;
+		blendingRanges.m_DestinationRanges = destinationRanges;
+	}
+	else if (colorMode == Enum::ColorMode::Grayscale)
+	{
+		Data sourceRanges;
+		Data destinationRanges;
+		for (int i = 0; i < 2u; ++i)
+		{
+			sourceRanges.push_back(std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(0u, 0u, 255u, 255u));
+			destinationRanges.push_back(std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(0u, 0u, 255u, 255u));
+			blendingRanges.m_Size += 8u;
+		}
+		blendingRanges.m_SourceRanges = sourceRanges;
+		blendingRanges.m_DestinationRanges = destinationRanges;
+	}
+	else if (colorMode == Enum::ColorMode::CMYK)
+	{
+		Data sourceRanges;
+		Data destinationRanges;
+		for (int i = 0; i < 6u; ++i)
+		{
+			sourceRanges.push_back(std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(0u, 0u, 255u, 255u));
+			destinationRanges.push_back(std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>(0u, 0u, 255u, 255u));
+			blendingRanges.m_Size += 8u;
+		}
+		blendingRanges.m_SourceRanges = sourceRanges;
+		blendingRanges.m_DestinationRanges = destinationRanges;
+	}
+	else
+	{
+		PSAPI_LOG_ERROR("Layer", "Currently only RGB, Grayscale and CMYK colormodes are supported for layer blending ranges")
+	}
+	return blendingRanges;
+}
 
 
 
--- PhotoshopAPI/src/LayeredFile/LayerTypes/Layer.h
+++ PhotoshopAPI/src/LayeredFile/LayerTypes/Layer.h
@@ -2,6 +2,7 @@
 
 #include "Macros.h"
 #include "Enum.h"
+#include "Struct/ImageChannel.h"
 #include "PhotoshopFile/LayerAndMaskInformation.h"
 
 #include <vector>
@@ -9,23 +10,55 @@
 
 PSAPI_NAMESPACE_BEGIN
 
+// Structure describing a layer mask (pixel based)
+template <typename T> 
+struct LayerMask
+{
+	ImageChannel<T> maskData;
+	bool isDisabled;
+	std::optional<uint8_t> maskDensity;
+	std::optional<float64_t> maskFeather;
+};
+
 /// Base Struct for Layers of all types (Group, Image, [Adjustment], etc.)
 /// Includes the bare minimum 
 template <typename T>
 struct Layer
 {
 	std::string m_LayerName;
-	std::vector<T> m_LayerMask;
+	std::optional<LayerMask<T>> m_LayerMask;
 	Enum::BlendMode m_BlendMode;
 
 	uint8_t m_Opacity;	// 0 - 255 despite the appearance being 0-100 in photoshop
 
-	uint64_t m_Width;
-	uint64_t m_Height;
+	uint32_t m_Width;
+	uint32_t m_Height;
+	int32_t m_CenterX;
+	int32_t m_CenterY;
 
-	Layer() : m_LayerName(""), m_LayerMask({}), m_BlendMode(Enum::BlendMode::Normal), m_Opacity(255), m_Width(0u), m_Height(0u) {};
+	Layer() : m_LayerName(""), m_LayerMask({}), m_BlendMode(Enum::BlendMode::Normal), m_Opacity(255), m_Width(0u), m_Height(0u), m_CenterX(0u), m_CenterY(0u) {};
 	Layer(const LayerRecord& layerRecord, const ChannelImageData& channelImageData);
+
+	// Each layer must implement a function that parses it to a global namespace
+	virtual std::tuple<LayerRecord, ChannelImageData> toPhotoshop(Enum::ColorMode colorMode) = 0;
 	virtual ~Layer() = default;
+protected:
+
+	// Generates the LayerMaskData struct from the layer mask (if provided)
+	std::optional<LayerRecords::LayerMaskData> generateMaskData();
+
+	// Generate the channel extents from the width, height and center coordinates
+	// returns top, left, bottom, right in that order
+	std::tuple<int32_t, int32_t, int32_t, int32_t> generateExtents();
+
+	// Generate the layer name as a pascal string
+	PascalString generatePascalString();
+
+	// Generate the layer blending ranges (which for now are just the defaults)
+	// We can do this here as in rgb mode the channels are always grey, r, g, b and alpha (?).
+	// The same holds true for other color modes where for greyscale e.g. it would be just grey and alpha (?)
+	// and cmyk would be grey, cyan, magenta, yellow, key and alpha (?)
+	LayerRecords::LayerBlendingRanges generateBlendingRanges(const Enum::ColorMode colorMode);
 };
 
 PSAPI_NAMESPACE_END
