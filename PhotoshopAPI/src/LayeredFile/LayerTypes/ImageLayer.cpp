cd01b0cc8d0345188276e07fc1fabce72db40de7
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2023-12-19 11:01:02 +0100

    Add a toPhotoshop method to the imageLayer

--- PhotoshopAPI/src/LayeredFile/LayerTypes/ImageLayer.cpp
+++ PhotoshopAPI/src/LayeredFile/LayerTypes/ImageLayer.cpp
@@ -1,7 +1,10 @@
 #include "ImageLayer.h"
 
 #include "Layer.h"
+#include "PhotoshopFile/LayerAndMaskInformation.h"
 
+#include <vector>
+#include <unordered_map>
 
 PSAPI_NAMESPACE_BEGIN
 
@@ -12,13 +15,61 @@ template struct ImageLayer<uint16_t>;
 template struct ImageLayer<float32_t>;
 
 
+
 // ---------------------------------------------------------------------------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------------
 template <typename T>
-ImageLayer<T>::ImageLayer(const LayerRecord& layerRecord, const ChannelImageData& channelImageData) : Layer<T>(layerRecord, channelImageData)
+std::tuple<LayerRecord, ChannelImageData> ImageLayer<T>::toPhotoshop(const Enum::ColorMode colorMode)
+{
+	PascalString lrName = Layer<T>::generatePascalString();
+	auto extents = Layer<T>::generateExtents();
+	int32_t top = std::get<0>(extents);
+	int32_t left = std::get<1>(extents);
+	int32_t bottom = std::get<2>(extents);
+	int32_t right = std::get<3>(extents);
+	uint16_t channelCount = m_ImageData.size() + static_cast<uint16_t>(m_LayerMask.has_value());
+	// Initialize the channelInfo. Note that if the data is to be compressed the channel size gets update
+	// again later
+	std::vector<LayerRecords::ChannelInformation> channelInfo;
+	for (const auto& it : m_ImageData)
+	{
+		channelInfo.push_back(LayerRecords::ChannelInformation{ it.first.id, it.second.m_OrigSize });
+	}
+	uint8_t clipping = 0u;	// No clipping mask for now
+	uint8_t bitFlags = 1u << 1;	// Set the layer to be visible
+	std::optional<LayerRecords::LayerMaskData> lrMaskData = Layer<T>::generateMaskData();
+	LayerRecords::LayerBlendingRanges blendingRanges = Layer<T>::generateBlendingRanges(colorMode);
+	
+	LayerRecord lrRecord = LayerRecord(
+		lrName,
+		top,
+		left,
+		bottom,
+		right,
+		channelCount,
+		channelInfo,
+		m_BlendMode,
+		m_Opacity,
+		clipping,
+		bitFlags,
+		lrMaskData,
+		blendingRanges,
+		std::nullopt	// We dont really need to pass any additional layer info in here
+	);
+	return lrRecord;
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+template <typename T>
+ImageLayer<T>::ImageLayer(const LayerRecord& layerRecord, const ChannelImageData& channelImageData) : 
+	Layer<T>(layerRecord, channelImageData)
 {
 	
 }
+
+
 
 
 PSAPI_NAMESPACE_END
--- PhotoshopAPI/src/LayeredFile/LayerTypes/ImageLayer.h
+++ PhotoshopAPI/src/LayeredFile/LayerTypes/ImageLayer.h
@@ -3,6 +3,7 @@
 #include "Macros.h"
 #include "Enum.h"
 #include "Layer.h"
+#include "Struct/ImageChannel.h"
 #include "PhotoshopFile/LayerAndMaskInformation.h"
 
 #include <unordered_map>
@@ -14,7 +15,10 @@ template <typename T>
 struct ImageLayer : public Layer<T>
 {
 	// Store the image data as a per-channel map to be used later
-	std::unordered_map<Enum::ChannelID, std::vector<T>> m_ImageData;
+	std::unordered_map<Enum::ChannelIDInfo, ImageChannel<T>> m_ImageData;
+
+	// Generate a photoshop layerRecords and imageData based on the current layer
+	std::tuple<LayerRecord, ChannelImageData> toPhotoshop(const Enum::ColorMode colorMode);
 
 	ImageLayer(const LayerRecord& layerRecord, const ChannelImageData& channelImageData);
 };
