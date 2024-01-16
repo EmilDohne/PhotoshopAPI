051f88b9c738ebaaa008857e585b2dbfbae55b63
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-01-16 18:09:48 +0100

    update example to clean it up a little

--- PhotoshopExamples/CreateSimpleDocument/main.cpp
+++ PhotoshopExamples/CreateSimpleDocument/main.cpp
@@ -4,21 +4,20 @@
 int main()
 {
 	// Initialize some constants that we will need throughout the program
-	const PhotoshopAPI::Enum::ColorMode colorMode = PhotoshopAPI::Enum::ColorMode::RGB;
-	const uint32_t width = 64u;
-	const uint32_t height = 64u;
+	const static PhotoshopAPI::Enum::ColorMode colorMode = PhotoshopAPI::Enum::ColorMode::RGB;
+	const static uint32_t width = 64u;
+	const static uint32_t height = 64u;
 
-	PhotoshopAPI::LayeredFile<uint8_t> document = PhotoshopAPI::LayeredFile<uint8_t>(colorMode, width, height);
+	PhotoshopAPI::LayeredFile<uint8_t> document = { colorMode, width, height };
 
-	// Create our individual channels to add to our image layer. Keep in mind that all 3 channels need to be specified
-	// for RGB mode
+	// Create our individual channels to add to our image layer. Keep in mind that all these 3 channels need to 
+	// be specified for RGB mode
 	std::unordered_map <PhotoshopAPI::Enum::ChannelID, std::vector<uint8_t>> channelMap;
-	channelMap[PhotoshopAPI::Enum::ChannelID::Red] = std::vector<uint8_t>(static_cast<uint64_t>(width) * height, 255u);
-	channelMap[PhotoshopAPI::Enum::ChannelID::Green] = std::vector<uint8_t>(static_cast<uint64_t>(width) * height, 0u);
-	channelMap[PhotoshopAPI::Enum::ChannelID::Blue] = std::vector<uint8_t>(static_cast<uint64_t>(width) * height, 0u);
+	channelMap[PhotoshopAPI::Enum::ChannelID::Red] = std::vector<uint8_t>(width * height, 255u);
+	channelMap[PhotoshopAPI::Enum::ChannelID::Green] = std::vector<uint8_t>(width * height, 0u);
+	channelMap[PhotoshopAPI::Enum::ChannelID::Blue] = std::vector<uint8_t>(width * height, 0u);
 
-	// We use a shared_ptr as this allows us to keep modifying this layer if we wanted.
-	std::shared_ptr<PhotoshopAPI::ImageLayer<uint8_t>> layer = std::make_shared<PhotoshopAPI::ImageLayer<uint8_t>>(
+	auto layer = std::make_shared<PhotoshopAPI::ImageLayer<uint8_t>>(
 		std::move(channelMap),					// The image channels for our image layer
 		std::nullopt,							// An optional mask channel
 		"Red Layer",							// The name of the layer.
@@ -32,7 +31,7 @@ int main()
 
 	document.addLayer(layer);
 
-	//Set some other properties on the layer. This will update the layer and we dont actually need to ask the LayeredFile instance for the layer again.
+	// It is perfectly legal to modify a layers properties even after it was added to the document
 	layer->m_Opacity = 128u;
 
 	// Convert to PhotoshopDocument and write to disk. Note that from this point onwards 
