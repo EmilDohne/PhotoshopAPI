4b6f8378f4cfab8d5d22151b2cd6c1b2e4ac0afa
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-01-24 21:04:06 +0100

    Add barebones structure

---
+++ PhotoshopExamples/AddLayerMasks/main.cpp
@@ -0,0 +1,16 @@
+/*
+Example of creating a simple document with a single layer using the PhotoshopAPI. The preferred method is always to interact through the LayeredFile
+struct rather than directly with the low level PhotoshopAPI::PhotoshopFile although that is also possible but requires a deep understanding of the
+underlying structure whereas LayeredFile abstracts the implementation details.
+*/
+
+#include "PhotoshopAPI.h"
+
+#include <unordered_map>
+#include <vector>
+
+
+int main()
+{
+
+}
