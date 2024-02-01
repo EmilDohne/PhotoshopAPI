b935934f585da6adf5444c5d136c0ea9b92fd7a0
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-02-01 21:54:37 +0100

    Mark Example correctly as TODO

--- PhotoshopExamples/AddLayerMasks/main.cpp
+++ PhotoshopExamples/AddLayerMasks/main.cpp
@@ -12,24 +12,5 @@ underlying structure whereas LayeredFile abstracts the implementation details.
 
 int main()
 {
-	NAMESPACE_PSAPI::Instrumentor::Get().BeginSession("PSAPI_Profile", "LargeReadWrite.json");
-
-	auto inputFile = PhotoshopAPI::File("./large_file.psb");
-	auto psDocumentPtr = std::make_unique<PhotoshopAPI::PhotoshopFile>();
-	psDocumentPtr->read(inputFile);
-
-
-	// In this case we already know the bit depth but otherwise one could use the PhotoshopFile.m_Header.m_Depth
-	// variable on the PhotoshopFile to figure it out programmatically
-	PhotoshopAPI::LayeredFile<PhotoshopAPI::bpp16_t> layeredFile = { std::move(psDocumentPtr) };
-
-	// We can now convert back to a PhotoshopFile and write out to disk
-	PhotoshopAPI::File::FileParams params = { .doRead = false, .forceOverwrite = true };
-	auto outputFile = PhotoshopAPI::File("./large_file_api.psb", params);
-	auto psOutDocumentPtr = PhotoshopAPI::LayeredToPhotoshopFile(std::move(layeredFile));
-
-	psOutDocumentPtr->write(outputFile);
-
-	NAMESPACE_PSAPI::Instrumentor::Get().EndSession();
-
+	// TODO 
 }
