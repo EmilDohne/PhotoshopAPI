453e5cc8515d7d936732e7c5d9f018ef077047c3
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-02-21 12:09:13 +0100

    update enums to numpy style docs

--- python/src/DeclareEnums.h
+++ python/src/DeclareEnums.h
@@ -14,27 +14,23 @@ using namespace NAMESPACE_PSAPI;
 
 void declareBitDepthEnum(py::module& m)
 {
-	py::enum_<Enum::BitDepth> bitDepth(m, "BitDepth", 
-		R"pbdoc(
+	py::enum_<Enum::BitDepth> bitDepth(m, "BitDepth", R"pbdoc(
 		Enum representing the bit depth of an image.
 
-        The supported bit depths are:
-			- ``bd_8``: 8 bits per channel.
-			- ``bd_16``: 16 bits per channel.
-			- ``bd_32``: 32 bits per channel.
+        Attributes
+		-------------
+
+		bd_8 : int
+			8-bits per channel, equivalent to numpy.uint8
+		bd_16 : int
+			16-bits per channel, equivalent to numpy.uint16
+		bd_32 : int
+			32-bits per channel, equivalent to numpy.float32
+
         )pbdoc");
-	bitDepth.value("bd_8", Enum::BitDepth::BD_8,
-		R"pbdoc(
-		8-bits per channel, equivalent to a numpy.uint8
-		)pbdoc");
-	bitDepth.value("bd_16", Enum::BitDepth::BD_16,
-		R"pbdoc(
-		16-bits per channel, equivalent to a numpy.uint16
-		)pbdoc");
-	bitDepth.value("bd_32", Enum::BitDepth::BD_32, 
-		R"pbdoc(
-		32-bits per channel, equivalent to a numpy.float32
-		)pbdoc");
+	bitDepth.value("bd_8", Enum::BitDepth::BD_8);
+	bitDepth.value("bd_16", Enum::BitDepth::BD_16);
+	bitDepth.value("bd_32", Enum::BitDepth::BD_32);
 	// Note that we do not expose the bd_1 enum
 	// for now as it isnt within the currently supported
 	// bit-depths
@@ -49,23 +45,20 @@ void declareColorModeEnum(py::module& m)
 		R"pbdoc(
 		Enum representing the color mode of an file.
 
-        The supported color modes are:
-			- ``rgb``: rgb color mode
+		Attributes
+		-------------
 
-		more will be added before the 1.0.0 release
+		rgb : int
+			rgb color mode (supports channels R, G, B and A)
+		cmyk : int
+			cmyk color mode (supports channels C, M, Y, K and A)
+		grayscale : int
+			grayscale color mode (supports channels Gray, A)
+
         )pbdoc");
-	colorMode.value("rgb", Enum::ColorMode::RGB,
-		R"pbdoc(
-		rgb color mode (supports channels R, G, B and A)
-		)pbdoc");
-	colorMode.value("cmyk", Enum::ColorMode::CMYK,
-		R"pbdoc(
-		cmyk color mode (supports channels C, M, Y, K and A)
-		)pbdoc");
-	colorMode.value("grayscale", Enum::ColorMode::Grayscale,
-		R"pbdoc(
-		grayscale color mode (supports channels Gray, A)
-		)pbdoc");
+	colorMode.value("rgb", Enum::ColorMode::RGB);
+	colorMode.value("cmyk", Enum::ColorMode::CMYK);
+	colorMode.value("grayscale", Enum::ColorMode::Grayscale);
 	// For now we dont document the others either
 
 	colorMode.export_values();
@@ -90,4 +83,7 @@ void declareChannelIDEnum(py::module& m)
 	channelID.value("custom", Enum::ChannelID::Custom);
 	channelID.value("mask", Enum::ChannelID::UserSuppliedLayerMask);
 	channelID.value("alpha", Enum::ChannelID::Alpha);
+
+	channelID.export_values();
+
 }
