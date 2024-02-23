18f49f0267bf7a431725d43c3ea4789a4617e686
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-02-23 11:38:21 +0100

    Add declarations to py module

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -3,13 +3,14 @@
 
 #include "DeclareLayeredFile.h"
 #include "DeclarePhotoshopFile.h"
+#include "DeclareLayer.h"
 #include "DeclareImageLayer.h"
+#include "DeclareGroupLayer.h"
 #include "DeclareEnums.h"
 #include "DeclareUtil.h"
 
 namespace py = pybind11;
 using namespace NAMESPACE_PSAPI;
-
 
 PYBIND11_MODULE(psapi, m) {
 	/*py::options options;
@@ -19,14 +20,24 @@ PYBIND11_MODULE(psapi, m) {
 	declareBitDepthEnum(enum_module);
 	declareColorModeEnum(enum_module);
 	declareChannelIDEnum(enum_module);
+	declareCompressionEnums(enum_module);
+	declareBlendModeEnum(enum_module);
 
 	auto util_module = m.def_submodule("util", "Utility functions and structures to support the creation/interaction with LayeredFile or PhotoshopFile");
 	declareFileStruct(util_module);
 	declareChannelIDInfo(util_module);
 
+	declareLayer<bpp8_t>(m, "_8bit");
+	declareLayer<bpp16_t>(m, "_16bit");
+	declareLayer<bpp32_t>(m, "_32bit");
+
 	declareImageLayer<bpp8_t>(m, "_8bit");
 	declareImageLayer<bpp16_t>(m, "_16bit");
 	declareImageLayer<bpp32_t>(m, "_32bit");
+
+	declareGroupLayer<bpp8_t>(m, "_8bit");
+	declareGroupLayer<bpp16_t>(m, "_16bit");
+	declareGroupLayer<bpp32_t>(m, "_32bit");
 
 	declareLayeredFile<bpp8_t>(m, "_8bit");
 	declareLayeredFile<bpp16_t>(m, "_16bit");
