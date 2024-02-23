532deb544ecf61c97ae2fa7d04abb37f94a989c6
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-02-23 11:36:46 +0100

    Expose more of the enums

--- python/src/DeclareEnums.h
+++ python/src/DeclareEnums.h
@@ -87,3 +87,74 @@ void declareChannelIDEnum(py::module& m)
 	channelID.export_values();
 
 }
+
+
+void declareCompressionEnums(py::module& m)
+{
+	py::enum_<Enum::Compression> compression(m, "Compression",
+		R"pbdoc(
+		Enum representation of all the different Compression codecs supported by Photoshop (and PSAPI).
+
+		Attributes
+		-------------
+
+		raw : int
+			encode as raw bytes (no compression)
+		rle : int
+			encode with run-length-encoding for fastest write speeds at the cost of lower compression ratios (especially for 16- and 32-bit)
+		zip : int
+			encode with zip (deflate) compression, usually the best compression codec choice as well as zipprediction
+		zipprediction : int
+			encode with zip (deflate) compression but additionally 'prediction' encode the data which takes the difference between the last and 
+			the current pixel per scanline and stores that (for 32-bit files it interleaves the bytes).
+		)pbdoc");
+
+	compression.value("raw", Enum::Compression::Raw);
+	compression.value("rle", Enum::Compression::Rle);
+	compression.value("zip", Enum::Compression::Zip);
+	compression.value("zipprediction", Enum::Compression::ZipPrediction);
+
+	compression.export_values();
+
+}
+
+
+void declareBlendModeEnum(py::module& m)
+{
+	py::enum_<Enum::BlendMode> blendMode(m, "BlendMode",
+		R"pbdoc(
+		Enum representation of all the different channel ids found in a file.
+		)pbdoc");
+
+	blendMode.value("passthrough", Enum::BlendMode::Passthrough);
+	blendMode.value("normal", Enum::BlendMode::Normal);
+	blendMode.value("dissolve", Enum::BlendMode::Dissolve);
+	blendMode.value("darken", Enum::BlendMode::Darken);
+	blendMode.value("multiply", Enum::BlendMode::Multiply);
+	blendMode.value("colorburn", Enum::BlendMode::ColorBurn);
+	blendMode.value("linearburn", Enum::BlendMode::LinearBurn);
+	blendMode.value("darkercolor", Enum::BlendMode::DarkerColor);
+	blendMode.value("lighten", Enum::BlendMode::Lighten);
+	blendMode.value("screen", Enum::BlendMode::Screen);
+	blendMode.value("colordodge", Enum::BlendMode::ColorDodge);
+	blendMode.value("lineardodge", Enum::BlendMode::LinearDodge);
+	blendMode.value("lightercolor", Enum::BlendMode::LighterColor);
+	blendMode.value("overlay", Enum::BlendMode::Overlay);
+	blendMode.value("softlight", Enum::BlendMode::SoftLight);
+	blendMode.value("hardlight", Enum::BlendMode::HardLight);
+	blendMode.value("vividlight", Enum::BlendMode::VividLight);
+	blendMode.value("linearlight", Enum::BlendMode::LinearLight);
+	blendMode.value("pinlight", Enum::BlendMode::PinLight);
+	blendMode.value("hardmix", Enum::BlendMode::HardMix);
+	blendMode.value("difference", Enum::BlendMode::Difference);
+	blendMode.value("exclusion", Enum::BlendMode::Exclusion);
+	blendMode.value("subtract", Enum::BlendMode::Subtract);
+	blendMode.value("divide", Enum::BlendMode::Divide);
+	blendMode.value("hue", Enum::BlendMode::Hue);
+	blendMode.value("saturation", Enum::BlendMode::Saturation);
+	blendMode.value("color", Enum::BlendMode::Color);
+	blendMode.value("luminosity", Enum::BlendMode::Luminosity);
+
+	blendMode.export_values();
+
+}
