61d5615b5f1141931710c7bd0b54451c27cf8c43
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-02-26 16:35:28 +0100

    Port all examples to python

---
+++ PhotoshopExamples/ModifyLayerStructure/modify_layer_structure.py
@@ -0,0 +1,33 @@
+# These are just for running these in the repository but is not necessary when the files are pip installed
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../bin-int/PhotoshopAPI/x64-release/python")))
+
+# Example of creating a simple document with a single layer using the PhotoshopAPI.
+import psapi
+import numpy as np
+
+
+def main() -> None:
+
+    # In the python bindings we expose a wrapper which reads a LayeredFile instance with the
+    # correct bit-depth
+    layered_file = psapi.LayeredFile.read(os.path.join(os.path.dirname(__file__), "LayeredFile.psd"))
+
+    # We can now perform any operations we want to, here we move the NestedGroup to the root of the scene by not specifying 
+    # a second parameter and then remove the ImageLayer (this does not remove the Group!)
+    layered_file.move_layer("Group/NestedGroup")
+    layered_file.remove_layer("Group/ImageLayer")
+
+    # We can also use objects to perform these operations. Note how the indexing reflects
+    # what we did in the last operation
+    nested_img_layer = layered_file["NestedGroup"]["NestedImageLayer"]
+    group_layer = layered_file["Group"]
+
+    # We now want to move the NestedImageLayer back under Group 
+    layered_file.move_layer(nested_img_layer, group_layer)
+
+    # Note the implicit conversion to PSB works smoothly without any additional work
+    layered_file.write(os.path.join(os.path.dirname(__file__), "RearrangedFile.psb"))
+
+if __name__ == "__main__":
+    main()
