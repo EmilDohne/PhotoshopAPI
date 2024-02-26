0e6ec550f986bc6cf9021efb47d3985b6d4ffccb
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-02-26 13:44:04 +0100

    (experimental) try building python bindings on rtd

--- docs/doxygen/conf.py
+++ docs/doxygen/conf.py
@@ -1,6 +1,24 @@
 # The sphinx template and setup was mostly adopted from openimageios docs to mimic their styling
 import subprocess, os, sys
 
+# This is for local development to properly include the paths
+sys.path.insert(0, os.path.abspath("../../bin-int/PhotoshopAPI/x64-release/python"))
+sys.path.insert(0, os.path.abspath("../../bin-int/PhotoshopAPI/x64-debug/python"))
+
+
+def buildPhotoshopAPI(build_dir: str) -> str:
+    '''
+    Build the photoshopAPI with python bindings so we can display them in our docs
+    '''
+    base_path = os.path.join(os.path.abspath(__file__), "../.." )
+    build_dir = os.path.join(os.path.abspath(__file__), build_dir)
+    print(f"Building cmake on dir '{base_path}' into dir '{build_dir}'")
+    
+    subprocess.run(["cmake", "-B", build_dir, "-DCMAKE_BUILD_TYPE=Release", "-DPSAPI_BUILD_DOCS=OFF", "-DPSAPI_BUILD_BENCHMARKS=OFF", "-DPSAPI_BUILD_TESTS=OFF"], cwd=base_path)
+    # run the build process
+    subprocess.run(["cmake", "--build", build_dir, "--config", "Release"])
+    return build_dir
+
 
 def configureDoxyfile(input_dir, output_dir):
     with open('Doxyfile.in', 'r') as file :
@@ -18,11 +36,18 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 breathe_projects = {}
 
 if read_the_docs_build:
+    print("Detected we are running in Readthedocs")
     input_dir = '../../PhotoshopAPI'
     output_dir = 'build'
     configureDoxyfile(input_dir, output_dir)
     subprocess.call('doxygen', shell=True)
     breathe_projects['PhotoshopAPI'] = output_dir + '/build'
+    
+    # Build the PSAPI for the python bindings and append the path so we can resolve it
+    bin_path = buildPhotoshopAPI("bin")
+    py_module_path = os.path.join(bin_path, "PhotoshopAPI/x64-release/python")
+    sys.path.insert(0, py_module_path)
+    
 
 
 # -- Project information -----------------------------------------------------
