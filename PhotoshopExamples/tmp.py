479a0c98f9588cb78065d43712f0a7b44df5b110
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-03-08 17:36:23 +0100

    add back numpy code

--- PhotoshopExamples/tmp.py
+++ PhotoshopExamples/tmp.py
@@ -10,9 +10,9 @@ print(color_mode)
 # Generate our LayeredFile instance
 document = psapi.LayeredFile_8bit(color_mode, width, height)
 
-# img_data = np.zeros((3, height, width), np.uint8)
-# img_data[0] = 255
-# mask = np.full((height, width), 128, np.uint8)
+img_data = np.zeros((3, height, width), np.uint8)
+img_data[0] = 255
+mask = np.full((height, width), 128, np.uint8)
 
 # # When creating an image layer the width and height parameter are required if its not a zero sized layer
 # img_layer = psapi.ImageLayer_8bit(img_data, "Layer Red", layer_mask=mask, width=width, height=height)
