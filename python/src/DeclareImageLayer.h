a5ab5332bf78f6f4c64e91052a51cd2c7527c4c4
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-03-08 19:02:12 +0100

    add some debug logging to imagelayer as well

--- python/src/DeclareImageLayer.h
+++ python/src/DeclareImageLayer.h
@@ -170,7 +170,7 @@ std::shared_ptr<ImageLayer<T>> createImageLayerFromNpArray(
     params.compression = compression;
     params.colorMode = color_mode;
     std::cout << "Preparing to generate image layer" << std::endl;
-    return std::make_shared<ImageLayer<T>>(img_data_cpp, params);
+    return std::make_shared<ImageLayer<T>>(std::move(img_data_cpp), params);
 }
 
 
