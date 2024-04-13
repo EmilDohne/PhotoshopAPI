34b9efc3df73dc7c616a4834b07aaf02a01085c6
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-04-13 17:26:44 +0200

    Rework prediction encoding with support for AVX2

--- PhotoshopAPI/src/Util/Compression/Compress_ZIP.h
+++ PhotoshopAPI/src/Util/Compression/Compress_ZIP.h
@@ -3,6 +3,7 @@
 #include "Macros.h"
 #include "Logger.h"
 #include "CompressionUtil.h"
+#include "InterleavedToPlanar.h"
 #include "Endian/EndianByteSwap.h"
 #include "Endian/EndianByteSwapArr.h"
 #include "Struct/ByteStream.h"
@@ -35,28 +36,24 @@ namespace ZIP_Impl
 	// ---------------------------------------------------------------------------------------------------------------------
 	// ---------------------------------------------------------------------------------------------------------------------
 	template <typename T>
-	std::vector<T> PredictionEncode(std::vector<T>& data, const uint32_t width, const uint32_t height)
+	void PredictionEncode(std::vector<T>& data, std::span<uint8_t> buffer, const uint32_t width, const uint32_t height)
 	{
 		PROFILE_FUNCTION();
-		// Here we must first prediction encode per scanline and then do the big endian conversion afterwards
-		// This operation unfortunately cannot be done in-place but we can use a temporary buffer to avoid unnecessary memory overhead
-		std::vector<T> buffer(width);
 		for (int y = 0; y < height; ++y)
 		{
-			// We must set the initial value manually as the prediction encoding goes from the first value
-			buffer[0] = data[static_cast<uint64_t>(width) * y];
+			// Initialize the prediction encoding for the current scanline
+			T prev = data[static_cast<uint64_t>(width) * y];
 			for (uint32_t x = 1; x < width; ++x)
 			{
-				// Generate the difference between the current value and the next one and store it
-				buffer[x] = data[static_cast<uint64_t>(width) * y + x] - data[static_cast<uint64_t>(width) * y + x - 1];
+				// Perform prediction encoding in-place
+				T current = data[static_cast<uint64_t>(width) * y + x];
+				data[static_cast<uint64_t>(width) * y + x] = current - prev;
+				prev = current;
 			}
-			uint64_t destOffset = static_cast<uint64_t>(width) * y;
-			std::memcpy(reinterpret_cast<void*>(data.data() + destOffset), reinterpret_cast<void*>(buffer.data()), width * sizeof(T));
 		}
 
+		// After performing prediction encoding, apply big-endian conversion
 		endianEncodeBEArray<T>(data);
-
-		return std::move(data);
 	}
 
 
@@ -65,66 +62,48 @@ namespace ZIP_Impl
 	// ---------------------------------------------------------------------------------------------------------------------
 	// ---------------------------------------------------------------------------------------------------------------------
 	template <>
-	inline std::vector<float32_t> PredictionEncode(std::vector<float32_t>& data, const uint32_t width, const uint32_t height)
+	inline void PredictionEncode(std::vector<float32_t>& data, std::span<uint8_t> buffer, const uint32_t width, const uint32_t height)
 	{
 		PROFILE_FUNCTION();
 
-		// Endian encode the data first unlike the regular prediction encoding. This is because we operate on a byte level here for 
-		// the prediction encoding rather than on the actual values
-		endianEncodeBEArray(data);
-
 		std::span<uint8_t> byteDataView(reinterpret_cast<uint8_t*>(data.data()), data.size() * sizeof(float32_t));
-
-		// First de-interleave the data to planar byte order, i.e. going from 1234 1234 1234 1234 to 1111 2222 3333 4444
-		// We essentially split each scanline into 4 equal parts each holding the first, second, third and fourth of the original bytes
-		// Additionally we endian encode the data by reversing the order in which we access the original bytes.
-		// TODO this can actually be done in-place with vectorization
 		std::vector<uint32_t> verticalIter = createVerticalImageIterator(height);
-		std::for_each(std::execution::par, verticalIter.begin(), verticalIter.end(),
-			[&](uint32_t y)
-			{
-				// this buffer will hold the interleaved bytes of each scanline
-				std::vector<uint8_t> buffer(width * sizeof(float32_t));
-				// convenient views into the individual sectors of the data
-				std::span<uint8_t> firstRowView(buffer.data() + width * 0, width);
-				std::span<uint8_t> secondRowView(buffer.data() + width * 1, width);
-				std::span<uint8_t> thirdRowView(buffer.data() + width * 2, width);
-				std::span<uint8_t> fourthRowView(buffer.data() + width * 3, width);
-
-				for (uint64_t x = 0; x < width; ++x)
+		{
+			PROFILE_SCOPE("32-bit binary de-interleave");
+			// First de-interleave the data to planar byte order, i.e. going from 1234 1234 1234 1234 to 1111 2222 3333 4444
+			// We essentially split each scanline into 4 equal parts each holding the first, second, third and fourth of the original bytes
+			// We also convert to big endian order which is what is stored on disk
+			std::for_each(std::execution::par, verticalIter.begin(), verticalIter.end(), [&](uint32_t y)
 				{
-					uint64_t rowCoord = static_cast<uint64_t>(y) * width * sizeof(float32_t);
+					// Binary view into the whole scanline
+					std::span<uint8_t> scanlineView(byteDataView.data() + y * width * sizeof(float32_t), width * sizeof(float32_t));
+					std::span<uint8_t> scanlineBuffer(buffer.data() + y * width * sizeof(float32_t), width * sizeof(float32_t));
+					interleavedToPlanarFloat(scanlineView, scanlineBuffer, width);
+					// Copy over the memory from the buffer into the data
+					std::memcpy(scanlineView.data(), scanlineBuffer.data(), scanlineView.size());
+				});
+		}
 
-					firstRowView[x] = byteDataView[rowCoord + x * sizeof(float32_t) + 0];	// Byte 0
-					secondRowView[x] = byteDataView[rowCoord + x * sizeof(float32_t) + 1];	// Byte 1
-					thirdRowView[x] = byteDataView[rowCoord + x * sizeof(float32_t) + 2];	// Byte 2
-					fourthRowView[x] = byteDataView[rowCoord + x * sizeof(float32_t) + 3];	// Byte 3
-				}
-
-				// Copy the row back over, no need to lock here as the ranges are not overlapping
-				std::memcpy(reinterpret_cast<void*>(data.data() + y * width), reinterpret_cast<void*>(buffer.data()), buffer.size());
-			});
-
-		// Perform the prediction encoding of the data, keep in mind that this is done byte by byte
-		std::for_each(std::execution::par, verticalIter.begin(), verticalIter.end(),
-			[&](uint32_t y)
-			{
-				std::vector<uint8_t> buffer(width * sizeof(float32_t));
-				// Generate a non-overlapping view of each row of the data
-				uint8_t* dataRowOffset = reinterpret_cast<uint8_t*>(data.data() + static_cast<uint64_t>(y) * width);
-				std::span<uint8_t> dataView(dataRowOffset, width * sizeof(float32_t));
-				// We must set the initial value manually as the prediction encoding goes from the first value
-				buffer[0] = dataView[0];
-				for (uint32_t x = 1; x < width * sizeof(float32_t); ++x)
+		{
+			PROFILE_SCOPE("32-bit binary prediction encode");
+			// Perform the prediction encoding of the data, keep in mind that this is done byte by byte
+			std::for_each(std::execution::par, verticalIter.begin(), verticalIter.end(),
+				[&](uint32_t y)
 				{
-					// Generate the difference between the current value and the next one and store it
-					buffer[x] = dataView[x] - dataView[x - 1];
-				}
-				std::memcpy(reinterpret_cast<void*>(dataView.data()), reinterpret_cast<void*>(buffer.data()), width * sizeof(float32_t));
-			});
+					// Generate a non-overlapping view of each row of the data
+					uint8_t* dataRowOffset = reinterpret_cast<uint8_t*>(data.data() + static_cast<uint64_t>(y) * width);
+					std::span<uint8_t> dataView(dataRowOffset, width * sizeof(float32_t));
 
-
-		return std::move(data);
+					uint8_t prev = dataView[0];
+					for (uint32_t x = 1; x < width * sizeof(float32_t); ++x)
+					{
+						uint8_t curr = dataView[x];
+						// Generate the difference between the current value and the next one and store it
+						dataView[x] = curr - prev;
+						prev = curr;
+					}
+				});
+		}
 	}
 
 
@@ -135,6 +114,7 @@ namespace ZIP_Impl
 	template <typename T>
 	std::vector<uint8_t> Compress(const std::vector<T>& uncompressedData, std::span<uint8_t> buffer, libdeflate_compressor* compressor)
 	{
+		PROFILE_FUNCTION();
 		std::vector<uint8_t> compressedData;
 		// These represent the header bytes of the zlib stream
 		const uint8_t compressionType = 0x78;
@@ -156,7 +136,6 @@ namespace ZIP_Impl
 		size_t inputBytes = uncompressedData.size() * sizeof(T);
 		size_t bytesUsed;
 		{
-			PROFILE_SCOPE("Zip Deflate");
 			bytesUsed = libdeflate_deflate_compress(compressor, inputBuffer, inputBytes,
 				buffer.data(), buffer.size());
 			if (bytesUsed == 0) {
@@ -244,11 +223,11 @@ std::vector<uint8_t> CompressZIPPrediction(std::vector<T>& uncompressedIn, std::
 {
 	PROFILE_FUNCTION();
 
-	// Prediction encode as well as byteswapping
-	std::vector<T> predictionEncodedData = ZIP_Impl::PredictionEncode<T>(uncompressedIn, width, height);
+	// Prediction encode as well as byteswapping in-place
+	ZIP_Impl::PredictionEncode<T>(uncompressedIn, buffer, width, height);
 
 	// Compress using Deflate ZIP
-	std::vector<uint8_t> compressedData = ZIP_Impl::Compress<T>(predictionEncodedData, buffer, compressor);
+	std::vector<uint8_t> compressedData = ZIP_Impl::Compress<T>(uncompressedIn, buffer, compressor);
 
 	return compressedData;
 }
@@ -263,15 +242,15 @@ std::vector<uint8_t> CompressZIPPrediction(std::vector<T>& uncompressedIn, const
 {
 	PROFILE_FUNCTION();
 
-	// Prediction encode as well as byteswapping
-	std::vector<T> predictionEncodedData = ZIP_Impl::PredictionEncode<T>(uncompressedIn, width, height);
-
 	// Allocate the compressor as well as a sufficiently large swap buffer
 	libdeflate_compressor* compressor = libdeflate_alloc_compressor(ZIP_COMPRESSION_LVL);
-	std::vector<uint8_t> buffer(libdeflate_zlib_compress_bound(compressor, predictionEncodedData.size() * sizeof(T)));
+	std::vector<uint8_t> buffer(libdeflate_zlib_compress_bound(compressor, uncompressedIn.size() * sizeof(T)));
+
+	// Prediction encode as well as byteswapping in-place
+	ZIP_Impl::PredictionEncode<T>(uncompressedIn, buffer, width, height);
 
 	// Compress using Deflate ZIP
-	std::vector<uint8_t> compressedData = ZIP_Impl::Compress<T>(predictionEncodedData, buffer, compressor);
+	std::vector<uint8_t> compressedData = ZIP_Impl::Compress<T>(uncompressedIn, buffer, compressor);
 
 	libdeflate_free_compressor(compressor);
 
