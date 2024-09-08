2c88b0aae8de6e8cd02ab632ce3f22df068e14d2
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-09-08 16:08:42 +0200

    update default Descriptor constructor as well as modifying hexdump.py to be more "production ready"

--- scripts/hexdump_section.py
+++ scripts/hexdump_section.py
@@ -1,9 +1,35 @@
+"""
+hexdump.py
+
+This script extracts a specified section of a binary file and saves it to a new file
+with the same base name but a .bin extension.
+
+Usage:
+    python hexdump.py <path> <offset> <size>
+
+Arguments:
+    <path>    : The path to the input file (e.g., smartobject.psd).
+    <offset>  : The offset (in bytes) from where to start reading the file.
+    <size>    : The number of bytes to read from the offset.
+
+Example:
+    python hexdump.py smartobject.psd 21966 1652
+
+This will read 1652 bytes from the file `smartobject.psd` starting at byte offset 21966
+and will write the output to `smartobject.bin` in the same directory.
+"""
 import os
+import argparse
 
 
 def hexdump_section(path: str, offset: int, size: int):
     '''
     Hexdump a file section to an equally named file with the extension .bin
+    
+    :param path: The path to the input file.
+    :param offset: The offset (in bytes) from where to start the dump.
+    :param size: The number of bytes to read from the offset.
+    """
     '''
     with open(path, mode='rb') as binary_file:
         binary_file.seek(offset)
@@ -13,5 +39,12 @@ def hexdump_section(path: str, offset: int, size: int):
             print (f"Wrote to file: {out_path}")
             out_file.write(buff)
 
+
 if __name__ == "__main__":
-    hexdump_section("C:/Users/emild/Desktop/smartobject.psd", 21966, 1652)
+    parser = argparse.ArgumentParser(description="Hexdump a section of a file to a new file with a .bin extension.")
+    parser.add_argument('path', type=str, help="The path to the input file.")
+    parser.add_argument('offset', type=int, help="The offset (in bytes) from where to start the dump.")
+    parser.add_argument('size', type=int, help="The number of bytes to read from the offset.")
+    
+    args = parser.parse_args()
+    hexdump_section(args.path, args.offset, args.size)
