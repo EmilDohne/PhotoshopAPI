f4548a7e416ca05dc7c9336b913f89b05188f014
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2024-10-15 16:42:50 +0200

    Add ReplaceImageData example

---
+++ PhotoshopExamples/ReplaceImageData/main.cpp
@@ -0,0 +1,53 @@
+/*
+Example of loading a PhotoshopFile and extracting the image data, modifying this and then replacing it again
+*/
+
+#include "PhotoshopAPI.h"
+
+#include <algorithm>
+#include <execution>
+#include <cmath>
+#include <cstdint>
+#include <vector>
+#include <unordered_map>
+
+
+// sRGB to linear conversion
+float srgbToLinear(uint8_t srgb)
+{
+	float normalized = srgb / 255.0f;
+	if (normalized <= 0.04045f)
+		return normalized / 12.92f;
+	else
+		return std::pow((normalized + 0.055f) / 1.055f, 2.4f);
+}
+
+int main()
+{
+	using namespace NAMESPACE_PSAPI;
+
+	// In this case we already know the bit depth but otherwise one could use the PhotoshopFile.m_Header.m_Depth
+	// variable on the PhotoshopFile to figure it out programmatically. This would need to be done using the 
+	// "extended" read signature shown in the ExtendedSignature example.
+	LayeredFile<bpp8_t> layeredFile = LayeredFile<bpp8_t>::read("ImageData.psb");
+
+	// We could also use findLayer() on the LayeredFile but this way we directly get the appropriate type.
+	// Keep in mind this can return nullptr!
+	auto imageLayerPtr = findLayerAs<bpp8_t, ImageLayer>("Blue_Lagoon/Blue_Lagoon.exr", layeredFile);
+
+	// Now we can grab all channels (we could also use just grab a single channel)
+	auto channels = imageLayerPtr->getImageData();
+
+	// Now we do our modifications. In this example we apply a sRGB -> linear operation
+	for (auto& [_, value] : channels)
+	{
+		std::for_each(std::execution::par, value.begin(), value.end(), [&](uint8_t& pixelValue) 
+			{
+				pixelValue = static_cast<uint8_t>(std::round(srgbToLinear(pixelValue) * 255.0f));
+			});
+	}
+
+	// Finally we can set the image data to the channel again and save out our file
+	imageLayerPtr->setImageData(std::move(channels));
+	LayeredFile<bpp8_t>::write(std::move(layeredFile), "ModifiedImageData.psb");
+}
