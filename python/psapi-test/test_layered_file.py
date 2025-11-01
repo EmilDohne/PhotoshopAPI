b03e5fa7b077bb18177067404436591d10a88941
EmilDohne <86836589+EmilDohne@users.noreply.github.com>
2025-11-01 13:51:49 +0100

    bug: fix #180 #182 by adding better unicode support to file read/write (#184)

---
+++ python/psapi-test/test_layered_file.py
@@ -0,0 +1,32 @@
+﻿import unittest
+import os
+import shutil
+import photoshopapi as psapi
+
+
+class TestLayeredFile(unittest.TestCase):
+    base_path = os.path.join(os.path.dirname(__file__), "documents", "BaseFile.psb")
+    copy_path = os.path.join(os.path.dirname(__file__), "documents", "你好.psd")
+    out_path = os.path.join(os.path.dirname(__file__), "documents", "你好_2.psd")
+
+    def test_unicode_characters(self):
+        # Copy and rename the file
+        shutil.copy2(self.base_path, self.copy_path)
+        self.assertTrue(os.path.exists(self.copy_path), "Failed to copy file to Unicode path")
+
+        # Attempt to open the new file
+        doc = psapi.LayeredFile.read(self.copy_path)
+        self.assertIsNotNone(doc, "Failed to open copied document")
+
+
+    def test_write_unicode_chars(self):
+        # Copy and rename the file
+        shutil.copy2(self.base_path, self.copy_path)
+        self.assertTrue(os.path.exists(self.copy_path), "Failed to copy file to Unicode path")
+
+        # Attempt to open the new file
+        doc = psapi.LayeredFile.read(self.copy_path)
+        self.assertIsNotNone(doc, "Failed to open copied document")
+
+        doc.write(self.out_path)
+        self.assertTrue(os.path.exists(self.out_path), "Failed to save file to Unicode path")
